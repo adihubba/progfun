e7abf1d3253f39108aea0910c33baa64adddeb81
adi <adi@fedora.fritz.box>
2016-09-02 23:27:21 +0200

    implement polynomial calculation

--- progfun2-calculator/src/main/scala/calculator/Polynomial.scala
+++ progfun2-calculator/src/main/scala/calculator/Polynomial.scala
@@ -1,13 +1,20 @@
 package calculator
 
 object Polynomial {
-  def computeDelta(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double]): Signal[Double] = {
-    ???
+  def computeDelta(a: Signal[Double], b: Signal[Double], c: Signal[Double]): Signal[Double] = {
+    Signal(b() * b() - 4.0 * a() * c())
   }
 
-  def computeSolutions(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+  def computeSolutions(a: Signal[Double], b: Signal[Double], c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
+    Signal({
+      val d = delta()
+      if (d < 0.0) {
+        Set()
+      } else {
+        val deltaSqrt = math.sqrt(delta())
+        Set((-b() + deltaSqrt) / (2.0 * a()),
+            (-b() - deltaSqrt) / (2.0 * a()))
+      }
+    })
   }
 }
