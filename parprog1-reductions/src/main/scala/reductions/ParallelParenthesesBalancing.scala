aac1847197552634f98a3e2f52775a99f6fe743e
adi <adi@fedora.fritz.box>
2016-12-26 23:30:03 +0100

    implement exercise 2

--- parprog1-reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
+++ parprog1-reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
@@ -38,25 +38,63 @@ object ParallelParenthesesBalancingRunner {
 
 object ParallelParenthesesBalancing {
 
-  /** Returns `true` iff the parentheses in the input `chars` are balanced.
+  /**
+   * Returns `true` iff the parentheses in the input `chars` are balanced.
    */
   def balance(chars: Array[Char]): Boolean = {
-    ???
+    val end = chars.length
+    def recursiveBalance(acc: Int, index: Int): Boolean = {
+      if (index >= end) acc == 0
+      else if (chars(index) == ')' && acc == 0) false
+      else {
+        val acc2 = chars(index) match {
+          case '(' => 1
+          case ')' => -1
+          case _ => 0
+        }
+        recursiveBalance(acc + acc2, index + 1)
+      }
+    }
+    
+    recursiveBalance(0,0);
+// version doesn't stop if 'acc' is the first time lower than 0
+//    if (chars.isEmpty) true
+//    else
+//      0 == chars
+//        .map({
+//        })
+//        ./:(0)((a, b) => if (a + b < 0) Int.MinValue else a + b)
+        
   }
 
-  /** Returns `true` iff the parentheses in the input `chars` are balanced.
+  /**
+   * Returns `true` iff the parentheses in the input `chars` are balanced.
    */
   def parBalance(chars: Array[Char], threshold: Int): Boolean = {
 
-    def traverse(idx: Int, until: Int, arg1: Int, arg2: Int) /*: ???*/ = {
-      ???
+    def traverse(idx: Int, until: Int, arg1: Int, arg2: Int): (Int, Int) = {
+      var minNest = 0
+      var totNest = 0
+      for (current <- idx until until) {
+        if (chars(current) == '(') totNest = totNest + 1
+        if (chars(current) == ')') totNest = totNest - 1
+        if (totNest < minNest) minNest = totNest
+      }
+
+      (minNest, totNest)
     }
 
-    def reduce(from: Int, until: Int) /*: ???*/ = {
-      ???
+    def reduce(from: Int, until: Int): (Int, Int) = {
+      if (until - from <= threshold) 
+        traverse(from, until, 0, 0)
+      else {
+        val middle = from + (until - from) / 2
+        val (resultLeft, resultRight) = parallel(reduce(from, middle), reduce(middle, until))
+        (Math.min(resultLeft._1, resultLeft._2 + resultRight._1), resultLeft._2 + resultRight._2)
+      }
     }
 
-    reduce(0, chars.length) == ???
+    reduce(0, chars.length) == (0, 0)
   }
 
   // For those who want more:
